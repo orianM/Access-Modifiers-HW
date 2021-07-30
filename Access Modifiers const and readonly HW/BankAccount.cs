a34b489eea598f43aff6044fb5ee43afd19c9def
orianM <86421120+orianM@users.noreply.github.com>
2021-07-30 17:59:36 +0300

    Add files via upload

---
+++ Access Modifiers const and readonly HW/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers_const_and_readonly_HW
+{
+    class BankAccount
+    {
+        private double balance;
+        private string bankName;
+        private readonly long accountNumber;
+
+        public BankAccount(double balance, string bankName, long accountNumber)
+        {
+            this.balance = balance;
+            this.bankName = bankName;
+            this.accountNumber = accountNumber;
+        }
+
+        public void WithdrawOrDeposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public string GetAccountDetails()
+        {
+            return $"Account number: {accountNumber}, Bank name: {bankName}";
+        }
+    }
+}
---
+++ Access Modifiers const and readonly HW/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers_const_and_readonly_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region question 1
+
+            /*Access levels:a. private - Only accessible by the class containing the field/function.
+                            b. public - Accessible to all and any project/assembly that has a reference to our assembly.
+                            c. protected - Accessible only to the inherating classes.
+                            d. internal - Only accessible by the current project/assembly.*/
+
+            #endregion
+
+            #region question 2
+
+            /*Access modifiers help us utilize the encapsulation and abstraction part of OOP(Object oriented programming)
+              by allowing us to decide what the user can see and what the programmer can see. We only give the user the abilites
+              we want him to be able to use.*/
+
+            #endregion
+
+            #region question 3
+
+            /*The Default Access Levels:a. class - internal.
+                                        b. members - private.
+                                        c. enum/interface - public.*/
+
+            BankAccount myAcc = new BankAccount(200.00D, "TheBank", 934537643);
+            //myAcc.balance = 2000000;
+            myAcc.WithdrawOrDeposit(70.00D);
+            Console.WriteLine($"{myAcc.GetAccountDetails()}");
+
+            #endregion
+        }
+    }
+}
